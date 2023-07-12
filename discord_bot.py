6af37adc4a3390fee8289d062cad296f298dc17a
LucaMenzio <46999301+LucaMenzio@users.noreply.github.com>
2023-07-12 15:52:08 +0200

    Updated discord_bot.py with new get_matchinfo command

--- discord_bot.py
+++ discord_bot.py
@@ -16,10 +16,11 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 Elo = Elo.AOE2ItaliaElo()    
+rlk = reliclink_API.relicAPI() 
 
 message = "Please check if the bot command is used correctly"
 
-#
+# first attempt to make a bot command, found online
 @bot.command(name='roll_dice', help='Simulates rolling dice. Wants the number of the dice and its sides as arguments')
 async def roll(ctx, number_of_dice, number_of_sides):
     dice = [
@@ -29,19 +30,21 @@ async def roll(ctx, number_of_dice, number_of_sides):
     await ctx.send(', '.join(dice))
 #---------------------------------------------------------------------
 
-#
+#---------------------------------------------------------------------
 @bot.command(name='elo1v1', help='Retrieves the 1v1 elo of the specified player name (please use "")')
 async def create_team(ctx, player):
     message = str(Elo.get_elo1v1(player))
     await ctx.send(message)
 
-#
+#---------------------------------------------------------------------
 @bot.command(name='elotg', help='Retrieves the tg elo of the specified player name (please use "")')
 async def create_team(ctx, player):
     message = str(Elo.get_elotg(player))
+    if player == "HSL | Loris":
+        message = "Il capo non ha Elo, ma l\'Elo ha un capo (scarso, comunque)"
     await ctx.send(message)
 
-#
+#---------------------------------------------------------------------
 @bot.command(name='create_team', help='Creates a balanced team given the players names (ex. !create_team "ITA | Born to be Brain" "OS | Piero" "ITA | Carma" "OS | Nessuno" )')
 async def create_team(ctx, *players):
     if(len(players) %2 != 0):
@@ -50,14 +53,31 @@ async def create_team(ctx, *players):
     team = Elo.balance_teams(players)
     message ="The best combination of players is " + str(team)
     await ctx.send(message)
+    
+#---------------------------------------------------------------------
+@bot.command(name='get_matchinfo', help='Retrieves the info about the last match played by the specified player')
+async def create_team(ctx, player, n_games):
 
-
-'''
-#
-@bot.command(name='matchinfo', help='Retrieves the info about the matches played by the specified player')
-async def create_team(ctx, player):
-
-    await ctx.send(message)
-'''
+    steam_ids = []
+    results = []
+    names = []
+    
+    in_steam_id = Elo.get_steam_id(player)
+    await ctx.send("Retrieving "+str(n_games)+" matches played by "+player+" with steam ID "+in_steam_id)
+    message = ""
+    steam_ids, names, results = rlk.getMatches(in_steam_id, int(n_games))
+    for i in range(len(steam_ids)):
+        if i == 0:
+            continue
+        message = "```"
+        message+= "Game number "+str(i)+"\n"
+        message+="+---------------------------------------+\n"
+        message+="\tPlayer | Result |\n"
+        message+="+---------------------------------------+\n"
+        for j in range(len(steam_ids[i])):
+            won = 'Won' if results[i][j] == 1 else 'Lost'
+            message+="\t"+str(names[i][j])+" \t\t "+str(won)+"\n"
+        await ctx.send(message+"```")
 
 bot.run(TOKEN)
+
